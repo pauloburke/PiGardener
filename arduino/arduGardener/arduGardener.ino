4aa21fe66819ccec4843970682dec6c211f6e1e8
Paulo Burke <burke@burke-note.burke-personal>
2019-10-01 11:41:51 -0300

    add sensors programming

--- arduino/arduGardener/arduGardener.ino
+++ arduino/arduGardener/arduGardener.ino
@@ -1,128 +1,191 @@
 #include <SoftwareSerial.h>
 
-#define RxD 2
+#define sensorInterrupt 0
+#define sensorPin 2
+#define RxD 4
 #define TxD 3
-#define LED1 5
 #define LED2 6
-#define LED3 7
-#define PUMP 9
-#define WS A0
-#define FLUX A2
+#define LED3 5
+#define PUMP 10
+#define WS 9
+#define BTN 11
 
 SoftwareSerial BTserial(RxD, TxD); // RX | TX
 
-//const int WS = A0;
 const long baudRate = 9600; 
-const int water_threshold = 700;
 const int water_check_interval = 1000;
+float calibrationFactor = 4.5;  
+
 
 char c=' ';
-String s="";
+String command = "";
 int have_water = 0;
 int last_water_check = 0;
 float vol = 0.0;
+volatile byte pulseCount;
 
 
-int check_water(int wt){
-  int measure = 0;
-  measure = analogRead(WS);
-  last_water_check = millis();
-  if((measure)< wt){
-    return(1);
-  }else{
-    return(0);
+void read_bluetooth(){ //
+  while(BTserial.available()){
+    c = BTserial.read();
+    command.concat(c);
+    if(c == '\r' || c == '\n'){ // find if there is carriage return
+      break;
+    }
+    while(!BTserial.available()){};
   }
-};
-
-int readFluxSensor(){
-  return(1);  
+  command = command.substring(0);
+  //if(command.length()>0) BTserial.println(command);
 }
 
-int check_pump(){
+int check_water(){
+  if(digitalRead(WS) == LOW) 
+  {
+    return(1);
+  } 
+  else 
+  {
+    return(0);
+  }
+}
+
+void pulseCounter()
+{
+  pulseCount++;
+}
+
+bool readFluxSensor(float v){
+
+  float ml_v = v*1000;  
+  float flowRate = 0.0;
+  unsigned int flowMilliLitres = 0;
+  unsigned long totalMilliLitres = 0;  
+  unsigned long oldTime = 0;
+  unsigned long initTime = millis();
+  pulseCount = 0;
+  
+  attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+
+  while(totalMilliLitres < ml_v && have_water){
+    if((millis() - initTime) > 1000*60*5) break;
+    if((millis() - oldTime) > 1000){
+        detachInterrupt(sensorInterrupt);
+        flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
+        oldTime = millis();
+        flowMilliLitres = (flowRate / 60) * 1000;
+        totalMilliLitres += flowMilliLitres;
+        pulseCount = 0;
+        attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+        have_water = check_water();
+    }    
+  }
+  detachInterrupt(sensorInterrupt);
+
+  return(totalMilliLitres >= ml_v);
+
+}
+
+bool check_pump(){
+  bool v = false;
   if(have_water){
-    int v = 0;
     digitalWrite(PUMP,HIGH);
     digitalWrite(LED3,HIGH);
-    delay(2000);
-    v = readFluxSensor();
+    v = readFluxSensor(0.1);
     digitalWrite(PUMP,LOW);
     digitalWrite(LED3,LOW);
-    if(v){
-      return(1);
-    }
   }
-  return(0);  
+  return(v);  
 }
 
 int pump_water(float volume){
+  bool flag = false;
   if(have_water){
-    float v = 0.0;
     digitalWrite(PUMP,HIGH);
     digitalWrite(LED3,HIGH);
-    while(have_water && v<volume){
-      have_water = check_water(water_threshold);
-      v += readFluxSensor();
-      delay(500);
-    }
+    flag = readFluxSensor(volume);
     digitalWrite(PUMP,LOW);
-    digitalWrite(LED3,LOW);
-    if(have_water){
-      return(1);
-    }    
+    digitalWrite(LED3,LOW);    
   }
-  return(0);
+  return(flag);
+}
+
+void check_leds(){
+  digitalWrite(LED2,HIGH);
+  digitalWrite(LED3,HIGH);
+  delay(1000);
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED3,LOW);
+  delay(400);
+  for(int i=0;i<10;i++){
+    digitalWrite(LED2,HIGH);
+    delay(100);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,HIGH);
+    delay(100);
+    digitalWrite(LED3,LOW);
+  }
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED3,LOW);
 }
 
 void setup() {
   
-  pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
   pinMode(PUMP, OUTPUT);
+  pinMode(WS, INPUT_PULLUP);
+  pinMode(BTN, INPUT_PULLUP);
+  pinMode(sensorPin, INPUT);
+  digitalWrite(sensorPin, HIGH);
 
   BTserial.begin(baudRate);
 
-  have_water = check_water(water_threshold);
-  check_pump();
+  check_leds();
+
+  have_water = check_water();
   
 }
 
-float btReadFloat(){
-  float n;
-  c = BTserial.read();
-  while(c!='\n'){
-    s+=c;
-    c = BTserial.read();
-  }
-  n = s.toFloat();
-  s="";
-  return(n);
-}
 
 void loop() {
 
   if((millis()-last_water_check) > water_check_interval){
-    have_water = check_water(water_threshold);
+    have_water = check_water();
     if(have_water){
       digitalWrite(LED2,HIGH);
     }else{
       digitalWrite(LED2,LOW);
     }
   }
+
+  if(digitalRead(BTN) == LOW) 
+  {
+    digitalWrite(LED3,HIGH);
+    digitalWrite(PUMP,HIGH);
+  } 
+  else 
+  {
+    digitalWrite(LED3,LOW);
+    digitalWrite(PUMP,LOW);
+  }
+
+  read_bluetooth();
   
-  if (BTserial.available()){
+  if (command.length()>0){
     
-    digitalWrite(LED1,HIGH);
-    
-    c = BTserial.read();
+    c = command[0];
 
     switch(c){
       
+      case 't':
+        BTserial.println("ok");
+        break;
+      
       case 'w':
         if(have_water){
-          BTserial.println("Tem agua");
+          BTserial.println("Have water");
         }else{
-          BTserial.println("Nao tem agua");
+          BTserial.println("No water");
         };
         break;
         
@@ -135,8 +198,8 @@ void loop() {
         break;
 
       case 'p':
-        
-        vol = btReadFloat();
+
+        vol = command.substring(1).toFloat();
                 
         if(check_pump()){
           BTserial.println("Pump Ok");
@@ -152,10 +215,9 @@ void loop() {
         }else{
           BTserial.println("Out of water!");
         }
+        break;
     }
-    
-  }else{
-    digitalWrite(LED1,LOW);
+    command = "";
   }
 
 }
